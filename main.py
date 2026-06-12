e97d56b77b274d5ada388dce3927f06dd82a6a45
Nivas Reddy <bn123@snu.edu.in>
2026-06-12 13:14:08 +0530

    deploying

--- main.py
+++ main.py
@@ -11,12 +11,11 @@ app = FastAPI(title="CarDekho AI Advisor", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://cardheko.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-app.mount("/assets", StaticFiles(directory="assets"), name="assets")
 
 app.include_router(router)
